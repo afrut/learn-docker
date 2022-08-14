64a9d79dbb441dd8a91a52fd71db98fed05c5c52
eric373 <25942990+eric373@users.noreply.github.com>
2022-08-13 20:20:12 -0400

    Use Go templates to format output of docker commands

--- scripts/docker-container-volume-list.ps1
+++ scripts/docker-container-volume-list.ps1
@@ -1,36 +1,18 @@
-# TODO: modify this to use quiet mode (-q) to return only id's.
-$volumes_ = docker volume ls
-$volumes = New-Object Collections.Generic.List[String]
-for($cntv = 1; $cntv -lt $volumes_.Length; $cntv++)
-{
-    # Last element of every line split by space is the volume-id
-    $volumes.Add($volumes_[$cntv].Split(" ")[-1])
-}
-
-$volumes = $volumes.ToArray()
+$volumes = docker volume ls -q
 
 $entries = New-Object Collections.Generic.List[Collections.HashTable]
 $volumes | ForEach-Object {
     # Last line of output from docker container ls is the line of interest
     # Split this line with " "
-    $result = $(docker container ls -a --filter volume=$_)
-
-    if($result.GetType() -eq [System.Object[]] -and $result.Length -ge 2)
+    $result = $(docker container ls -a --filter volume=$_ --format "{{json .}}")
+    if($result -ne $null)
     {
-        $result = $result[-1].Split(" ")
-        $ls = New-Object Collections.Generic.List[String]
-        for($cnt = 0; $cnt -lt $result.Length; $cnt++)
-        {
-            if($result[$cnt] -ne ""){
-                    $ls.Add($result[$cnt])
-            }
-        }
-                
+        $result = ConvertFrom-Json $result -AsHashtable
         $entries.Add(@{"volume-id" = $_
-            ;"container-id" = $ls[0]
-            ;"image-name" = $ls[1]
-            ;"container-name" = $ls[-1]
-        })
+                ;"container-id" = $result["ID"]
+                ;"image-name" = $result["Image"]
+                ;"container-name" = $result["Names"]
+            })
     }
 }
 
